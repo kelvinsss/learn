decdd08c25aed5f459cf00d93457747cc130325a
kelvinsss <guo_chun_0203@163.com>
2016-07-11 12:02:02 +0800

    commit

--- Data-Syn/src/main/java/cn/huimin/erpplat/constants/TaskSignalConstant.java
+++ Data-Syn/src/main/java/cn/huimin/erpplat/constants/TaskSignalConstant.java
@@ -7,7 +7,6 @@ public class TaskSignalConstant {
 
     public static volatile int DATA_SYN_SIGNAL = TaskStatusConstant.DATA_SYN_CLOSED;
 
-
     public static volatile int DATA_SYN_ERROR_SIGNAL = TaskStatusConstant.DATA_SYN_ERROR_CLOSED;
 
 }
